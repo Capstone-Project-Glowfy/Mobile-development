27826402ffd0afc5a0f1ea3378098ac79b16a36f
farhanfath <farhanfathur73@gmail.com>
2024-06-18 10:37:29 +0700

    docs: adding readme demo image

--- app/src/main/java/com/bangkit/glowfyapp/view/home/fragments/clinic/ClinicFragment.kt
+++ app/src/main/java/com/bangkit/glowfyapp/view/home/fragments/clinic/ClinicFragment.kt
@@ -1,7 +1,6 @@
 package com.bangkit.glowfyapp.view.home.fragments.clinic
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -15,8 +14,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
